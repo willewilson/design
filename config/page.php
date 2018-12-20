7c1116991fde31b878e6ba72805b3449bedc510a
willewilson <william_n@live.com>
2018-12-20 15:31:50 +0100

    ändring test

--- config/page.php
+++ config/page.php
@@ -3,7 +3,7 @@
  * Configuration file for page which can create and put together web pages
  * from a collection of views. Through configuration you can add the
  * standard parts of the page, such as header, navbar, footer, stylesheets,
- * javascripts and more.
+ * javascripts and more..
  */
 return [
     // This layout view is the base for rendering the page, it decides on where
